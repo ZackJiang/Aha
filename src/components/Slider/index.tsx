8411663cf2922f8f6dfdeae03d083bc0502364f1
ZackJiangNoodoe <zackjiang@noodoe.com>
2024-01-03 14:56:35 +0800

    update slider mobile view

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import Slider from '@mui/material/Slider';
 import styled from '@emotion/styled';
+import { useMediaQuery } from '@mui/material';
 
 interface ValueToDataMap {
   [key: number]: number;
 }
 
-const markValueToDataIndex: ValueToDataMap = {
+const desktopMarkValueToDataIndex: ValueToDataMap = {
   3: 0,
   11.72: 1,
   20.21: 2,
@@ -15,7 +16,16 @@ const markValueToDataIndex: ValueToDataMap = {
   50: 5,
 };
 
-const markValueToLabelValue: ValueToDataMap = {
+const mobileMarkValueToDataIndex: ValueToDataMap = {
+  3: 0,
+  11.44: 1,
+  19.47: 2,
+  28.15: 3,
+  36.18: 4,
+  50: 5,
+};
+
+const desktopMarkValueToLabelValue: ValueToDataMap = {
   3: 3,
   11.72: 6,
   20.21: 9,
@@ -24,7 +34,16 @@ const markValueToLabelValue: ValueToDataMap = {
   50: 50,
 };
 
-const marks = [
+const mobileMarkValueToLabelValue: ValueToDataMap = {
+  3: 3,
+  11.44: 6,
+  19.47: 9,
+  28.15: 12,
+  36.18: 15,
+  50: 50,
+};
+
+const DesktopMarks = [
   {
     value: 3,
     label: '3',
@@ -43,6 +62,33 @@ const marks = [
   },
   {
     value: 38.51,
+    label: '15',
+  },
+  {
+    value: 50,
+    label: '50',
+  },
+];
+
+const mobileMarks = [
+  {
+    value: 3,
+    label: '3',
+  },
+  {
+    value: 11.44,
+    label: '6',
+  },
+  {
+    value: 19.47,
+    label: '9',
+  },
+  {
+    value: 28.15,
+    label: '12',
+  },
+  {
+    value: 36.18,
     label: '15',
   },
   {
@@ -122,14 +168,31 @@ interface CustomSliderProps {
 function CustomSlider(props: CustomSliderProps) {
   const { onChange } = props;
   const [activeDataIndex, setActiveDataIndex] = useState<number>(0);
+  const isSmallScreen = useMediaQuery('(max-width:600px)');
 
   const handleSliderChange = (event: Event, newValue: number | number[]) => {
     if (Array.isArray(newValue)) {
-      onChange(markValueToLabelValue[newValue[0]]);
-      setActiveDataIndex(markValueToDataIndex[newValue[0]]);
+      onChange(
+        isSmallScreen
+          ? mobileMarkValueToLabelValue[newValue[0]]
+          : desktopMarkValueToLabelValue[newValue[0]],
+      );
+      setActiveDataIndex(
+        isSmallScreen
+          ? mobileMarkValueToDataIndex[newValue[0]]
+          : desktopMarkValueToDataIndex[newValue[0]],
+      );
     } else {
-      onChange(markValueToLabelValue[newValue]);
-      setActiveDataIndex(markValueToDataIndex[newValue]);
+      onChange(
+        isSmallScreen
+          ? mobileMarkValueToLabelValue[newValue]
+          : desktopMarkValueToLabelValue[newValue],
+      );
+      setActiveDataIndex(
+        isSmallScreen
+          ? mobileMarkValueToDataIndex[newValue]
+          : desktopMarkValueToDataIndex[newValue],
+      );
     }
   };
 
@@ -138,7 +201,7 @@ function CustomSlider(props: CustomSliderProps) {
       min={3}
       max={50}
       step={null}
-      marks={marks}
+      marks={isSmallScreen ? mobileMarks : DesktopMarks}
       onChange={handleSliderChange}
       activeDataIndex={activeDataIndex}
     />
