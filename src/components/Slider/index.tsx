c8fc911a85c24612d72ac270f680f827ae41223d
ZackJiangNoodoe <zackjiang@noodoe.com>
2024-01-06 11:21:49 +0800

    Refactor slider component

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -2,9 +2,15 @@ import { useState } from 'react';
 import Slider from '@mui/material/Slider';
 import styled from '@emotion/styled';
 import { useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
 interface ValueToDataMap {
   [key: number]: number;
+}
+
+interface Mark {
+  value: number;
+  label: string;
 }
 
 const desktopMarkValueToDataIndex: ValueToDataMap = {
@@ -43,7 +49,7 @@ const mobileMarkValueToLabelValue: ValueToDataMap = {
   50: 50,
 };
 
-const DesktopMarks = [
+const DesktopMarks: Mark[] = [
   {
     value: 3,
     label: '3',
@@ -70,7 +76,7 @@ const DesktopMarks = [
   },
 ];
 
-const mobileMarks = [
+const mobileMarks: Mark[] = [
   {
     value: 3,
     label: '3',
@@ -129,6 +135,18 @@ const StyledSlider = styled(Slider)<{ activeDataIndex: number }>`
     }
   }
 
+  ${(props: { activeDataIndex: number }) =>
+    props.activeDataIndex === 0 &&
+    `.MuiSlider-thumb {
+      left: 5px !important;
+    }`}
+
+  ${(props: { activeDataIndex: number }) =>
+    props.activeDataIndex === 5 &&
+    `.MuiSlider-thumb {
+      left: calc(100% - 10px) !important;
+    }`}
+
   .MuiSlider-mark {
     display: none;
   }
@@ -156,7 +174,6 @@ const StyledSlider = styled(Slider)<{ activeDataIndex: number }>`
       color: white;
       opacity: 1;
     }
-    
   `}
 `;
 
@@ -167,33 +184,20 @@ interface CustomSliderProps {
 
 function CustomSlider(props: CustomSliderProps) {
   const { onChange } = props;
+  const theme = useTheme();
   const [activeDataIndex, setActiveDataIndex] = useState<number>(0);
-  const isSmallScreen = useMediaQuery('(max-width:600px)');
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
   const handleSliderChange = (event: Event, newValue: number | number[]) => {
-    if (Array.isArray(newValue)) {
-      onChange(
-        isSmallScreen
-          ? mobileMarkValueToLabelValue[newValue[0]]
-          : desktopMarkValueToLabelValue[newValue[0]],
-      );
-      setActiveDataIndex(
-        isSmallScreen
-          ? mobileMarkValueToDataIndex[newValue[0]]
-          : desktopMarkValueToDataIndex[newValue[0]],
-      );
-    } else {
-      onChange(
-        isSmallScreen
-          ? mobileMarkValueToLabelValue[newValue]
-          : desktopMarkValueToLabelValue[newValue],
-      );
-      setActiveDataIndex(
-        isSmallScreen
-          ? mobileMarkValueToDataIndex[newValue]
-          : desktopMarkValueToDataIndex[newValue],
-      );
-    }
+    const value = Array.isArray(newValue) ? newValue[0] : newValue;
+    const labelValue = isSmallScreen
+      ? mobileMarkValueToLabelValue[value]
+      : desktopMarkValueToLabelValue[value];
+    const dataIndex = isSmallScreen
+      ? mobileMarkValueToDataIndex[value]
+      : desktopMarkValueToDataIndex[value];
+    onChange(labelValue);
+    setActiveDataIndex(dataIndex);
   };
 
   return (
